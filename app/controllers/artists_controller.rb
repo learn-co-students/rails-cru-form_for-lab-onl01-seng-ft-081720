6f9dbd37dd6a1b8ee24acf320bfdc6d8eccc0d0f
mtrimner <matt.trimner@gmail.com>
2020-10-17 16:33:58 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,30 @@
+class ArtistsController < ApplicationController
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def update
+
+    end
+
+    def show
+
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+end
